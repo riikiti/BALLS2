ad4ccaff91e7c8e133f4c4de6806ef364f295a01
ivanh <rik.chanel@gmail.com>
2022-09-26 21:28:43 +0300

    new game add

--- app/src/main/java/com/example/balls2/Result.java
+++ app/src/main/java/com/example/balls2/Result.java
@@ -2,7 +2,9 @@ package com.example.balls2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class Result extends AppCompatActivity {
@@ -19,5 +21,16 @@ public class Result extends AppCompatActivity {
         }
         TextView tA4 = findViewById(R.id.textView7);
         tA4.setText(String.valueOf(steps));
+    }
+
+    public void NewGame(View view) {
+        int[][] arr = {{1, 0, 1},
+                {0, 0, 0},
+                {0, 1, 0},};
+        int steps = 0;
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("steps",steps);
+        intent.putExtra("arr",arr);
+        startActivity(intent);
     }
 }
