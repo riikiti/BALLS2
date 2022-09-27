9e8547c47132531a4db6b8a97776fc07a725a696
ivanh <rik.chanel@gmail.com>
2022-09-27 22:23:54 +0300

    ii logic todo

---
+++ app/src/main/java/com/example/balls2/Stack.java
@@ -0,0 +1,30 @@
+package com.example.balls2;
+
+public class Stack {
+
+    private int mSize; //mSize - максимальный размер
+    private int[] stackArray;
+    private int top;
+
+    public Stack(int m) {
+        this.mSize = m;
+        stackArray = new int[mSize];
+        top = -1;
+    }
+    public void addElement(int element) {
+        stackArray[++top] = element;
+    }
+
+    public int deleteElement() {
+        return stackArray[top--];
+    }
+    public int readTop() {
+        return stackArray[top];
+    }
+    public boolean isEmpty() {
+        return (top == -1);
+    }
+    public boolean isFull() {
+        return (top == mSize - 1);
+    }
+}
